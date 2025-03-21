52ec1f7f3bf76218803915f752796581440837e2
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-22 01:34:34 +0300

    Initial commit

---
+++ src/main/java/com/demo/springsecurityblogappmuhammedduzgun/SpringsecurityblogappmuhammedduzgunApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springsecurityblogappmuhammedduzgun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityblogappmuhammedduzgunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityblogappmuhammedduzgunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springsecurityblogappmuhammedduzgun/SpringsecurityblogappmuhammedduzgunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springsecurityblogappmuhammedduzgun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityblogappmuhammedduzgunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
