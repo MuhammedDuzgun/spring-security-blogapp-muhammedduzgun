6860f03067eb7bf7b35ca0af1011d70b3893a21e
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-23 03:40:16 +0300

    handle BadCredentialsException

--- src/main/java/com/demo/springsecurityblogappmuhammedduzgun/exception/GlobalExceptionHandler.java
+++ src/main/java/com/demo/springsecurityblogappmuhammedduzgun/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.demo.springsecurityblogappmuhammedduzgun.exception;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +24,14 @@ public class GlobalExceptionHandler {
         apiErrorResponse.setMessage(ex.getMessage());
         apiErrorResponse.setStatus(HttpStatus.CONFLICT.value());
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadCredentialsException(BadCredentialsException ex){
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
+        apiErrorResponse.setMessage(ex.getMessage());
+        apiErrorResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception.class)
