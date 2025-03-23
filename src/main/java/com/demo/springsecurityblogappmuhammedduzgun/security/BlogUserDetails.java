d65cfee6f2b49064b54830cebb1a388a8cb2041f
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-23 03:42:11 +0300

    create BlogUserDetails

---
+++ src/main/java/com/demo/springsecurityblogappmuhammedduzgun/security/BlogUserDetails.java
@@ -0,0 +1,34 @@
+package com.demo.springsecurityblogappmuhammedduzgun.security;
+
+import com.demo.springsecurityblogappmuhammedduzgun.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class BlogUserDetails implements UserDetails {
+
+    private final User user;
+
+    public BlogUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+}
